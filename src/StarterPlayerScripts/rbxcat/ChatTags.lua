ec40dc35e6c53d3381f082772de966d081341e63
fartg <catfoodbeer@gmail.com>
2024-01-11 10:56:10 -0500

    add ChatTags module into StarterPlayerScripts

---
+++ src/StarterPlayerScripts/rbxcat/ChatTags.lua
@@ -0,0 +1,20 @@
+local TextChatService = game:GetService("TextChatService")
+local AdminEvents = game.ReplicatedStorage.Events.Admin;
+
+TextChatService.OnIncomingMessage = function(message: TextChatMessage)
+	if not message.TextSource then return end;
+
+	local _player = game.ReplicatedStorage.Players:FindFirstChild(message.TextSource.UserId);
+
+	if AdminEvents.CheckAdmin:InvokeServer() then
+		local props = Instance.new("TextChatMessageProperties");
+		props.PrefixText = "<font color='#FF0000'>[A]</font> " .. message.PrefixText
+		return props
+	end
+	
+	if _player:GetAttribute("vip") then 
+		local props = Instance.new("TextChatMessageProperties");
+		props.PrefixText = "<font color='#F5CD30'>[VIP]</font> " .. message.PrefixText
+		return props
+	end;
+end
